0e0db3305877d40da7f5bae8f3d8196a7b40bb91
Marvyn Kiplagat <marvynrogony@gmail.com>
2023-03-23 13:49:20 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector('#create-task-form');
+  const ul = document.querySelector('#tasks');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const userInput = document.querySelector("#new-task-description").value;
+
+    const listItem = document.createElement("li");
+    listItem.textContent = userInput;
+
+    ul.appendChild(listItem);
+
+    form.reset();
+    });
 });
